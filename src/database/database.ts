c9bdd3936a3ed2eb3eb0b41b486fde2dd508d40f
Mir Sahib <mirsahib24@gmail.com>
2023-04-11 15:30:47 +0600

    :card_file_box: Update database url

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,19 +1,23 @@
 import { MongoClient } from "mongodb";
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
+// MONGODB_URI=mongodb
+// MONGOHOST=kynet_database
+// MONGOPORT=27017
+// MONGOUSER=root
+// MONGOPASSWORD=root
+const uri = process.env.MONGO_URL || "mongodb://root:root@kynet_database:27017/kynet"
+const client = new MongoClient(uri);
 
-const uri = process.env.MONGO_URI || ''
-const client = new MongoClient(uri) 
+const connect = async () => {
+	try {
+		console.log("mongo_uri", uri);
+		await client.connect();
+		console.log(" Database connection established successfully");
+	} catch (error) {
+		console.log("Mongodb connection error:" + error);
+	}
+};
+connect();
 
-const connect = async()=>{
-    try {
-        console.log('mongo_uri',uri)
-        await client.connect()
-        console.log(" Database connection established successfully")
-    } catch (error) {
-        console.log("Mongodb connection error:"+error)
-    }
-}
-connect()
-
-export default client
+export default client;
